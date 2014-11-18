c955018a4be461703c95518d58b78b4322948bbf
Adam <wsfreehunter@gmail.com>
2014-11-18 18:27:50 +0100

    What is this? :D

--- src/org/mcstats/Metrics.java
+++ src/org/mcstats/Metrics.java
@@ -23,7 +23,7 @@
  *
  * The views and conclusions contained in the software and documentation are those of the
  * authors and contributors and should not be interpreted as representing official policies,
- * either expressed or implied, of anybody else.
+ * either expressed or implied, of anybody else. 
  */
 package org.mcstats;
 
